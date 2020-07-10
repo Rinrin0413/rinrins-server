489858a97233f08f2317363c1ae3fbdc85c41465
Rinrin0413 <67325022+Rinrin0413@users.noreply.github.com>
2020-07-10 18:45:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(function() {
     $("#title").fadeIn(2000);
     $("#subtitle").fadeIn(2700);
+    $("#discord-join-button").click(function(){
+        $("#discord-join-button").css("box-shadow", "0px 3px rgb(0, 116, 67)");
+        $("#discord-join-button").text("Joined");
+        $("#discord-join-button").css("left", "1420px");
+    });
 });
