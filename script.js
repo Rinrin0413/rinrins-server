0ce8f2f0c6d5dc2e5236269816c471dac1952a5c
Rinrin0413 <67325022+Rinrin0413@users.noreply.github.com>
2020-07-21 19:34:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+var $discord_join_button = $("#discord-join-button");
 $(function() {
     $("#title").fadeIn(2000);
     $("#subtitle").fadeIn(2700);
     $("#discord-join-button").click(function(){
-        $("#discord-join-button").css("box-shadow", "0px 3px rgb(0, 116, 67)");
-        $("#discord-join-button").text("|=+=< Joined >=+=|");
+        $discord_join_button.css("box-shadow", "0px 3px rgb(0, 116, 67)");
+        $discord_join_button.text("|=+=< Joined >=+=|");
+    });
+});
+
+$(function() {
+    $("#top_move").on('click touchstart', function () {
+        $('html,body').animate({
+             scrollTop: 0 
+            }, 'swing');
+        return false;
     });
 });
